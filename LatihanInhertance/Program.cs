5925f101f642d41f15cdf84158d938d74a782754
dewikartikaa <dwikrtka1818@gmail.com>
2019-05-07 14:53:41 +0700

    LatihanInhertance

---
+++ LatihanInhertance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInhertance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mike", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas", 30, "190302", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Sara", 19, "19506", "sara1232@amikom.ac.id");
+            student.GetNameAndAge();
+
+
+            Console.ReadKey();
+        }
+    }
+}
